1af13a0ae4fc208cba8dbe350e45586523d47a76
Rishabh sharma <rish6696.rs@gmail.com>
2019-05-25 16:58:33 +0530

    sec commit

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express=require('express');
 const app=express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+const PORT=process.env.PORT||4578
 
 app.use('/put',require('./write'))
 app.use('/get',require('./read'))
+app.get('/',(req,res)=>{
+    console.log("welcome");
 
-app.listen(7745,()=>{
+})
+
+app.listen(PORT,()=>{
     console.log("server started");
 })
