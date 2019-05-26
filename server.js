f18d5acafb5e3267084b2846d60408223b5371da
Rishabh sharma <rish6696.rs@gmail.com>
2019-05-26 14:27:17 +0530

    added uri frm youtue

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT=process.env.PORT||4578
 app.use('/put',require('./write'))
 app.use('/get',require('./read'))
 app.get('/',(req,res)=>{
-    console.log(Db_Url);
+    
     res.send("welcome");
 
 })
